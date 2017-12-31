5f439511ffa8e10f4968990c4338f99a7c4d948f
Nestor Gomez Jimenez <gomez315@umn.edu>
2017-12-30 23:55:19 -0600

    updated database.sql file to Server folder

--- server/database.sql
+++ server/database.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE personal-site;
 
-CREATE TABLE "users" (
-  "id" serial primary key,
-  "username" varchar(80) not null UNIQUE,
-  "password" varchar(240) not null
-);
+CREATE TABLE users (
+id SERIAL PRIMARY KEY,
+username varchar(55),
+password varchar(60));
+
 
