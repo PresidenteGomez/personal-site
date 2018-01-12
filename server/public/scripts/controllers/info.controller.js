aff05a778356d407b8847a380b1f77a42c24ab8b
Nestor Gomez Jimenez <gomez315@umn.edu>
2018-01-12 17:05:08 -0500

    new working nav

--- server/public/scripts/controllers/info.controller.js
+++ server/public/scripts/controllers/info.controller.js
@@ -3,21 +3,22 @@ myApp.controller('InfoController', function(UserService) {
   var vm = this;
   vm.userService = UserService;
 
-  window.onscroll = function () { scrollFunction()
-  };
-    function scrollFunction() {
-      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-      } else {
-        document.getElementById("myBtn").style.display = "none";
-      }
-    }
+  // window.onscroll = function () { 
+  //   scrollFunction() 
+  // };
+  //   function scrollFunction() {
+  //     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  //       document.getElementById("myBtn").style.display = "block";
+  //     } else {
+  //       document.getElementById("myBtn").style.display = "none";
+  //     }
+  //   }
 
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    }
+  //   // When the user clicks on the button, scroll to the top of the document
+  //   function topFunction() {
+  //     document.body.scrollTop = 0;
+  //     document.documentElement.scrollTop = 0;
+  //   }
 
 
 });
